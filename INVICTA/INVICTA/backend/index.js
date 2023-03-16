71f0c3f3877399d31dd6f86c15ae487ecd240c1b
Yash Jaiswal <bitorsic@protonmail.com>
2023-03-16 21:45:22 +0530

    Email database entry handled

--- INVICTA/INVICTA/backend/index.js
+++ INVICTA/INVICTA/backend/index.js
@@ -1,9 +1,11 @@
-const { sequelize } = require('./models')
+const { sequelize, Scores } = require('./models')
 const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+// app.use(express.static('public'))
+// app.use(express.static('views'))
 
 async function main() {
   try {
@@ -16,6 +18,21 @@ async function main() {
   }
 }
 
-main()
+// main()
+
+// app.get("/register", async (req, res) => {
+//   res.render("home trivia");
+// });
+
+app.post("/register", async (req, res) =>{
+  try {
+    const participant = await Scores.create({ email: req.body.email})
+
+    res.send({ success: true });
+  } catch (e) { 
+    res.status(500).send(e); 
+  }
+})
+
 
 app.listen(3000)
