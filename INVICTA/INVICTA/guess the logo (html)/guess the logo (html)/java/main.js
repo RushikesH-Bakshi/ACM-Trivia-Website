514e8bf4dca54ced52cacd7702cf67be69b181ac
iamprathmesh <86964450+iamprathmesh@users.noreply.github.com>
2023-03-17 12:42:03 +0530

    Update main.js

--- INVICTA/INVICTA/guess the logo (html)/guess the logo (html)/java/main.js
+++ INVICTA/INVICTA/guess the logo (html)/guess the logo (html)/java/main.js
@@ -9,13 +9,9 @@ var submit_btn = document.querySelector(".submit-btn"),
   title_data = "";
   var score = 0;
 
-//const history = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
-//var images = ['a.png', 'b.png', 'c.png', 'd.png', 'e.png','f.png', 'g.png', 'h.png', 'i.png', 'j.png', 'l.png', 'm.png', 'n.png', 'o.png', 'p.png','aa.jpg', 'h.jpg','k.jpg','m.jpg', 'n.jpg', 'o.jpg','r.jpg', 'x.jpg', 'y.jpg', 'z.jpg', 'p.webp','u.webp'];
-//var answer = ["angular","opel","blackberry","vuescript","webpack","redux","purescript","rust","dart","elm","lisp","morris garages","capgemini","wipro","cisco","perl","nbc","atlassian","unilever","toyota","audi","redbull","emporio armani","target","olay"];
-
-const history = [0,1,2,3,4,5,6,7];
-var images = ['a.png', 'b.png', 'c.png', 'd.png', 'e.png','f.png', 'g.png', 'h.png'];
-var answer = ["angular","opel","blackberry","vuescript","webpack","redux","purescript","rust"];
+const history = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+var images = ['a.png', 'b.png', 'c.png', 'd.png', 'e.png','f.png', 'g.png', 'h.png', 'i.png', 'j.png', 'l.png', 'm.png', 'n.png', 'o.png', 'p.png','aa.jpg', 'h.jpg','k.jpg','m.jpg', 'n.jpg', 'o.jpg','r.jpg', 'x.jpg', 'y.jpg', 'z.jpg', 'p.webp','u.webp','chevron.jpg','versace.jpg','unesco.jpg','dropbox.jpg','IEE.jpg'];
+var answer = ["angular","opel","blackberry","vuescript","webpack","redux","purescript","rust","dart","elm","lisp","morris garages","capgemini","wipro","cisco","perl","nbc","atlassian","unilever","toyota","audi","redbull","emporio armani","target","olay",'chevron','versae','unesco','dropbox','IEE'];
 
 window.addEventListener('load', () => {
     next();
